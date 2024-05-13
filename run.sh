0291f34457b05889ce184be344448cc77478b907
louisebphansen <louise.bp@hotmail.com>
2024-05-13 21:02:00 +0200

    update data structure

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
+# activate virtual environment
 source env/bin/activate
 
+# unzip data file (create /in folder with data)
 unzip data.zip
 
+# vectorize data
 python3 src/vectorize.py
 
+# run logistic regression classifier
 python3 src/logistic_classification.py
 
+# run neural network classification
 python3 src/nn_classification.py --hidden_layer_sizes 50
 
+# deactivate venv
 deactivate
